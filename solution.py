6fa060ba40fc275bb8e44642db657adb2d13ba63
Nadya <106408597+progNK@users.noreply.github.com>
2023-04-07 17:19:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,6 +10,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    loc1 = 2 / 92 ** 2 * (gamma.ppf((1 - p) / 2, x.size) / x.size + x.mean() - 1 / 2)
+    loc2 = 2 / 92 ** 2 * (gamma.ppf((1 + p) / 2, x.size) / x.size + x.mean() - 1 / 2)
 
-    return 2/92 ** 2 * (gamma.ppf((1-p)/2, x.size) / x.size + x.mean() - 1/2), \
-           2/92 ** 2 * (gamma.ppf((1+p)/2, x.size) / x.size + x.mean() - 1/2)
+    return loc1, loc2
